980318f744ddabda76b3cf2968a5735785137a24
glucaci <gabriel.lucaci.dev@gmail.com>
2022-10-28 14:24:06 +0200

    Fix

--- .build/Build.GitRepository.cs
+++ .build/Build.GitRepository.cs
@@ -4,5 +4,4 @@ using Nuke.Common.Tools.GitVersion;
 partial class Build
 {
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion(Framework = Net60)] readonly GitVersion GitVersion;
 }
